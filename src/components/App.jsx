d3fbbec5ee0879a66cea4179fc6380aae4c03a1b
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-24 12:04:08 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-
-const element = React.createElement('div', { a: 5, b: 10, children: ['Hello world!', 'Hello React!', 'Hello new abilities!'] });
-console.log(element);
-
-// ReactDOM.render(element, document.querySelector('#root'));
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
+    <div style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '100vh',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
+      }}>
+        {/* <Profile /> */}
     </div>
   );
 };
