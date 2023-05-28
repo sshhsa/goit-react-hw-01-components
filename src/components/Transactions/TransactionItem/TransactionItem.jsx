d4de0a72efd67064bb933f22b3bc9032afac9c9f
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-28 23:00:24 +0300

    commit
    excellent styles for the statistics from the index.css

--- src/components/Transactions/TransactionItem/TransactionItem.jsx
+++ src/components/Transactions/TransactionItem/TransactionItem.jsx
@@ -6,9 +6,9 @@ import css from '../TransactionHistory/TransactionHistory.module.css';
 function TransactionItem({ type, amount, currency }) {
     return (
         <tr className={css.tableParentBody}>
-            <td className={css.tableItem}>{type}</td>
-            <td className={css.tableItem}>{amount}</td>
-            <td className={css.tableItem}>{currency}</td>
+            <td className={css.type}>{type}</td>
+            <td className={css.amount}>{amount}</td>
+            <td className={css.currency}>{currency}</td>
         </tr>
     );
 }
