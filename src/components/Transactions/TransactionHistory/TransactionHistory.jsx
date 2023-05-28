c5dfe1747b4dc9ffa3654032b3786b93d541e653
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-28 16:28:05 +0300

    task 4 is completed

--- src/components/Transactions/TransactionHistory/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory/TransactionHistory.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import 'App.js';
-// import TransactionItem from '../TransactionItem/TransactionItem';
+import TransactionItem from '../TransactionItem/TransactionItem';
 import css from './TransactionHistory.module.css';
 
-function TransactionHistory({transactions}) {
+function TransactionHistory({items}) {
     return (
         <table className={css.transactionHistory}>
             <thead>
@@ -16,28 +16,23 @@ function TransactionHistory({transactions}) {
                 </tr>
             </thead>
             <tbody>
-                {/* {transactions.map((({ id, type, amount, currency }) => {
+                {items.map((({ id, type, amount, currency }) => {
                 return <TransactionItem key={id} type={type} amount={amount} currency={currency} />
-                }))} */}
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                }))}
             </tbody>
         </table>
   );
 };
 
-// TransactionHistory.propTypes = {
-//   transactions: PropTypes.arrayOf(
-//     PropTypes.shape({
-//         id: PropTypes.string.isRequired,
-//         type: PropTypes.string.isRequired,
-//         amount: PropTypes.number.isRequired,
-//         currency: PropTypes.number.isRequired
-//     })
-//   ).isRequired
-// };
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 export default TransactionHistory;
--- src/components/Transactions/TransactionItem/TransactionItem.jsx
+++ src/components/Transactions/TransactionItem/TransactionItem.jsx
@@ -1,16 +1,16 @@
-// import React from 'react';
+import React from 'react';
 
-// import 'App.js';
-// import css from './TransactionItem.module.css';
+import 'App.js';
+import css from '../TransactionHistory/TransactionHistory.module.css';
 
-// function TransactionItem({ type, amount, currency }) {
-//     return (
-//         <tr className={css.tableParentBody}>
-//             <td className={css.tableItem}>{type}</td>
-//             <td className={css.tableItem}>{amount}</td>
-//             <td className={css.tableItem}>{currency}</td>
-//         </tr>
-//     );
-// }
+function TransactionItem({ type, amount, currency }) {
+    return (
+        <tr className={css.tableParentBody}>
+            <td className={css.tableItem}>{type}</td>
+            <td className={css.tableItem}>{amount}</td>
+            <td className={css.tableItem}>{currency}</td>
+        </tr>
+    );
+}
 
-// export default TransactionItem;
+export default TransactionItem;
