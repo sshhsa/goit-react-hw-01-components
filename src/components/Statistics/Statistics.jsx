c672e6a754457321a950f473bdf08c6a3e488d80
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-28 22:22:59 +0300

    refact styles for tables and statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -15,7 +15,7 @@ function Statistics({ title, stats }) {
           {stats.map(item => (
             <li key={item.id} className={css.item}>
                 <span className={css.label}>{item.label}</span>
-                <span className={css.percentage}>{item.percentage}</span>
+                <span className={css.percentage}>{item.percentage}%</span>
             </li>
           ))}
         </ul>
